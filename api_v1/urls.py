f904fb4d5b7f952f9ae152132555d0d930889162
Alex Malykh <alexeyheather@gmail.com>
2021-05-14 17:14:40 +0700

    added router for viewset

--- api_v1/urls.py
+++ api_v1/urls.py
@@ -1,14 +1,16 @@
-from tasks.models import Task
-from django.urls import path
+from django.urls import path, include
 from django.conf.urls import url
 
 from rest_framework import permissions
+from rest_framework.routers import DefaultRouter
+
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 
 from .views import (
     MediaList, MediaDetail, TaskViewSet, AppFunctionList, SubfunctionList
 )
+
 
 schema_view = get_schema_view(
     openapi.Info(
@@ -22,34 +24,28 @@ schema_view = get_schema_view(
     permission_classes=(permissions.AllowAny,),
 )
 
-task_list = TaskViewSet.as_view({
-    'get': 'list',
-    'post': 'create'
-})
-task_detail = TaskViewSet.as_view({
-    'get': 'retrieve',
-    'put': 'update'
-})
+# Viewset router
+router = DefaultRouter()
+router.register(r'tasks', TaskViewSet)
 
 urlpatterns = [
+    # Router
+    path('', include(router.urls)),
+
+    # Documentation / OpenAPI
     url(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(
         cache_timeout=0), name='schema-json'
     ),
     url(r'^swagger/$', schema_view.with_ui(
         'swagger', cache_timeout=0), name='schema-swagger-ui'
     ),
-    url(r'^redoc/$', schema_view.with_ui(
-        'redoc', cache_timeout=0), name='schema-redoc'
-    ),
 
-    path('media/', MediaList.as_view(), name='media_list'),
-    path('media/<int:pk>/', MediaDetail.as_view(), name='media_detail'),
+    # Api endpoints
+    path('media/', MediaList.as_view(), name='media-list'),
+    path('media/<int:pk>/', MediaDetail.as_view(), name='media-detail'),
 
-    path('tasks/', task_list, name='task_list'),
-    path('tasks/<int:pk>/', task_detail, name='task_detail'),
-
-    path('functions/', AppFunctionList.as_view(), name='function_list'),
-    path('subfunctions/', SubfunctionList.as_view(), name='subfunction_list'),
+    path('functions/', AppFunctionList.as_view(), name='function-list'),
+    path('subfunctions/', SubfunctionList.as_view(), name='subfunction-list'),
 ]
 
 
